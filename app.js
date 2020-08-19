ff640a834b8e5f69230ca7cef67a3c6bb328e30a
vladyani <vladyslavyantsov@gmail.com>
2020-08-19 14:46:40 +0200

    chore: add eslint, prettier config

--- app.js
+++ app.js
@@ -1,8 +1,31 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const { graphqlHttp } = require('express-graphql');
+const { buildSchema } = require('graphql');
 
 const app = express();
 
 app.use(bodyParser.json());
 
-app.listen(3000);  
+app.use(
+  '/graphql',
+  graphqlHttp({
+    schema: buildSchema(`
+        type RootQuery {
+        
+        }
+
+        type RootMutation {
+
+        }
+
+        schema {
+            query: RootQuery
+            mutation: RootMutation
+        }
+    `),
+    rootValue: {},
+  })
+);
+
+app.listen(3000);
